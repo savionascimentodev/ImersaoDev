c4d737d2c01807c7b353a6249da3e43872d960fa
savionascimentodev <savionascimentodev@gmail.com>
2021-09-16 18:20:55 -0400

    ✨ Starting project one

---
+++ ConversorDeMoedas/script.js
@@ -0,0 +1,54 @@
+function converterBitCoin() {
+
+}
+
+function converterDolar() {
+
+}
+
+function converterEuro() {
+
+}
+// function Converter() {
+//   //Conversor em Dolar para Real
+//   let valorElemento = document.getElementById("valor");
+//   let valor = valorElemento.value;
+//   let valorEmDolarNumerico = parseFloat(valor);
+//   //console.log(valorEmDolarNumerico);
+
+//   let valorEmReal = valorEmDolarNumerico * 5.2;
+//   //console.log(valorEmReal);
+
+//   let elementoValorConvertido = document.getElementById("valorConvertido");
+//   let valorConvertido = "O resultado de DOLAR Para o Real é R$ " + valorEmReal;
+//   elementoValorConvertido.innerHTML = valorConvertido;
+// }
+// function Converter2() {
+//   //Conversor em Euro para Real
+//   let valorElemento = document.getElementById("valor2");
+//   let valor = valorElemento.value;
+//   let valorEmEuroNumerico = parseFloat(valor);
+//   //console.log(valorEmEuroNumerico);
+
+//   let valorEmReal = valorEmEuroNumerico * 6.18;
+//   //console.log(valorEmReal);
+
+//   let elementoValorConvertido = document.getElementById("ValorConvertidoEuro");
+//   let valorConvertido = "O resultado de EURO Para o Real é R$ " + valorEmReal;
+//   elementoValorConvertido.innerHTML = valorConvertido;
+// }
+// function ConverterBit() {
+//   //Conversor De BITCOIN para Real
+//   let valorElemento = document.getElementById("valorBitCoin");
+//   let valor = valorElemento.value;
+//   let valorEmBitcoin = parseFloat(valor).toFixed(1);
+//   //console.log(valorEmBitcoin);
+
+//   let valorEmReal = valorEmBitcoin * 247.987;
+//   //console.log(valorEmReal);
+
+//   let elementoValorConvertido = document.getElementById("ValorBitCoin");
+//   let valorConvertidoBitCoin =
+//     "O Resultado de BitCoin Para o Real é R$ " + valorEmReal;
+//   elementoValorConvertido.innerHTML = valorConvertidoBitCoin;
+// }
