c54d0cd1c676f3fe0051a09d783c6abcd1f9d350
savionascimentodev <savionascimentodev@gmail.com>
2021-09-25 17:59:49 -0400

    ✨ Updated Project 03

---
+++ AluraFlix/script.js
@@ -0,0 +1,9 @@
+// var listaFilmes = ["https://br.web.img3.acsta.net/pictures/17/11/06/14/13/5164749.jpg?coixp=50&coiyp=47", "https://media.fstatic.com/SYM7W903-AAwJUXwLGsXr6LtIHw=/290x478/smart/media/movies/covers/2014/07/bojack-horseman-season-1_tNone.jpg", "https://media.fstatic.com/4bJKKFE2GhptgCtZOQNo6rcFHmE=/290x478/smart/media/movies/photos/2018/07/final-space_t238443.jpg"]
+
+// https://static.wikia.nocookie.net/dublagem/images/5/5e/JujutsuK_Poster1.png/revision/latest?cb=20210220124602&path-prefix=pt-br JUJUTSU
+
+// https://cdn.culturagenial.com/imagens/death-note-cartaz.jpg DEATHNOTE
+
+// https://i.imgur.com/dVM8ffa.jpg SHINGEKI
+
+// https://sm.ign.com/ign_br/tv/f/fullmetal-/fullmetal-alchemist-brotherhood-1_qzyx.jpg FULLMETAL
